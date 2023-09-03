e977e84d53115a604b4f936c66e6c71ca4ed4461
surabhi <surabhi.das@outlook.com>
2023-09-03 14:17:17 -0400

    working on indv acc details

--- Durant-Younger-trust/src/main/webapp/app/entities/user-account/user-account-routing.module.ts
+++ Durant-Younger-trust/src/main/webapp/app/entities/user-account/user-account-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserAccountMainpageComponent} from './user-account-mainpage/user-account-mainpage.component'
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
+import {BankAccountDetailComponent} from "../bank-account/detail/bank-account-detail.component";
+import {BankAccountRoutingResolveService} from "../bank-account/route/bank-account-routing-resolve.service";
+import {IndvAccountDetailComponent} from "./indv-account-detail/indv-account-detail.component";
 
 const bankTransactionRoute: Routes = [
   {
@@ -10,10 +13,13 @@ const bankTransactionRoute: Routes = [
     canActivate: [UserRouteAccessService],
   },
   {
-    path: 'edit',
-    component: UserAccountMainpageComponent,
-    canActivate: [UserRouteAccessService]
-  }
+    path: ':id/view',
+    component: IndvAccountDetailComponent,
+    resolve: {
+      bankAccount: BankAccountRoutingResolveService,
+    },
+    canActivate: [UserRouteAccessService],
+  },
 ];
 
 @NgModule({
