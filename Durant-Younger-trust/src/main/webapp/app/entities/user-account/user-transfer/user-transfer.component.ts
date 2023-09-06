b95dc9201098c000dfa5b1d3f018132e907d3f7a
NigelBynum <136828778+NigelBynum@users.noreply.github.com>
2023-09-06 18:49:54 -0400

    working on transfer button drop downs

--- Durant-Younger-trust/src/main/webapp/app/entities/user-account/user-transfer/user-transfer.component.ts
+++ Durant-Younger-trust/src/main/webapp/app/entities/user-account/user-transfer/user-transfer.component.ts
@@ -26,12 +26,6 @@ export class UserTransferComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // when it routs, its gets the bank account user from whoerver is logged in
-// Fetch user's accounts from your service
-    // Replace this with your actual code to fetch accounts
-    // this.bankAccountService.getUserAccounts().subscribe((accounts) => {
-    //   this.accounts = accounts;
-    // });
     this.activatedRoute.data.subscribe(({ bankUser }) => {
       this.bankUser = bankUser;
     });
@@ -45,25 +39,8 @@ export class UserTransferComponent implements OnInit {
       const toAccount = this.transferForm.get('toAccount')?.value;
       const amount = this.transferForm.get('amount')?.value;
 
-      // Perform the money transfer logic here
-      // You should update the account balances accordingly
-
-      // Example code (you need to replace it with your actual logic):
       if (fromAccount && toAccount && amount) {
-        // const fromAccountObj = this.accounts.find((account) => account.id === fromAccount);
-        // const toAccountObj = this.accounts.find((account) => account.id === toAccount);
 
-        // if (fromAccountObj && toAccountObj) {
-        //   if (fromAccountObj.balance >= amount) {
-        //     fromAccountObj.balance -= amount;
-        //     toAccountObj.balance += amount;
-        //
-        //     // Update the account balances in your database using your service
-        //     // This is a placeholder; replace it with your actual service call
-        //     this.bankAccountService.updateAccount(fromAccountObj).subscribe();
-        //     this.bankAccountService.updateAccount(toAccountObj).subscribe();
-
-        // Optionally, you can display a success message
 
         console.log('Money transferred successfully.');
       } else {
